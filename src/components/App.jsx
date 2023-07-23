fa2c6807590624ee827546f9b73eb938b80cb168
PlutovaKate <114903556+PlutovaKate@users.noreply.github.com>
2023-07-23 18:39:35 +0300

    finish homework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,26 @@
+import { Component } from 'react';
+import css from './App.module.css';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-export const App = () => {
-  return (
-    <div>
-      <Searchbar />
-      <ImageGallery />
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    searchValue: '',
+  };
+
+  handleFormSubmit = searchValue => {
+    this.setState({ searchValue });
+  };
+
+  render() {
+    return (
+      <div className={css.app}>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        <ImageGallery searchValue={this.state.searchValue} />
+        <ToastContainer autoClose={2000} position="top-right" theme="dark" />
+      </div>
+    );
+  }
+}
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,11 +1,34 @@
 import { Component } from 'react';
 import css from './Searchbar.module.css';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import PropTypes from 'prop-types';
 
 class Searchbar extends Component {
+  state = {
+    searchValue: '',
+  };
+
+  handleInputChange = event => {
+    this.setState({ searchValue: event.currentTarget.value.toLowerCase() });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    if (this.state.searchValue.trim() === '') {
+      toast('Write something to search');
+      return;
+    }
+
+    this.props.onSubmit(this.state.searchValue);
+    this.setState({ searchValue: '' });
+  };
+
   render() {
     return (
       <header className={css.searchbar}>
-        <form className={css.searchForm}>
+        <form onSubmit={this.handleSubmit} className={css.searchForm}>
           <button type="submit" className={css.searchFormButton}>
             <span className={css.searchFormButtonLabel}>Search</span>
           </button>
@@ -13,9 +36,12 @@ class Searchbar extends Component {
           <input
             className={css.searchFormInput}
             type="text"
+            name="searchValue"
             autocomplete="off"
             autofocus
             placeholder="Search images and photos"
+            value={this.state.searchValue}
+            onChange={this.handleInputChange}
           />
         </form>
       </header>
@@ -24,3 +50,7 @@ class Searchbar extends Component {
 }
 
 export default Searchbar;
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
